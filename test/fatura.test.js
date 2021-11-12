1c1254c3f125c80e7d5a6f2bf5531b831d8fe175
Zyon <zyonamadeus01@gmail.com>
2021-11-12 02:49:50 -0300

    teste 2 refatorado

--- test/fatura.test.js
+++ test/fatura.test.js
@@ -48,6 +48,6 @@ describe('Fatura', () => {
     
         faturas = filtrarFaturaAcimaDeDoisMil(faturas);
 
-        expect(faturas).toEqual(faturas);
+        expect(faturas).toEqual(retornoEsperado);
     })
 });
